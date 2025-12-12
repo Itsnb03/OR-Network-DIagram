f6f3d9a4f4cd0e1a34bea461394360cfcaecc11f
Itsnb03 <nitinbharadwaj47@gmail.com>
2025-12-12 23:00:49 +0530

    Added Flask backend and network calculation logic

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 activities = [
-    {"id": "A", "duration": 3,"predecessors": []},
+    {"id": "A", "duration": 3, "predecessors": []},
+    {"id": "B", "duration": 2, "predecessors": ["A"]},
+    
     
 ]
